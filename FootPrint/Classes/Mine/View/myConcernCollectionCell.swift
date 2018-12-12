f8d5d0992c24536f5be8755cd18386faee2745f8
Justin Zhao <justin0101@gmail.com>
2018-12-13 00:20:38 +0800

    优化collection列表
    优化collection列表

--- FootPrint/Classes/Mine/View/myConcernCollectionCell.swift
+++ FootPrint/Classes/Mine/View/myConcernCollectionCell.swift
@@ -7,20 +7,33 @@
 //
 
 import UIKit
+import Kingfisher
 
 class myConcernCollectionCell: UICollectionViewCell, RegisterCell {
 
     @IBOutlet weak var aevterName: UILabel!
     @IBOutlet weak var aevterImageView: UIImageView!
+    @IBOutlet weak var IsVerifyView: UIImageView!
+    @IBOutlet weak var tipsButton: UIButton!
     var myConcernCollectionData : myConcernModel?{
         didSet{
-            aevterName.text = myConcernCollectionData?.name            
+            aevterName.text = myConcernCollectionData?.name
+            aevterImageView.kf.setImage(with: URL(string: (myConcernCollectionData?.icon)!))
+            
+            if let IsVerify = myConcernCollectionData?.is_verify{
+                IsVerifyView.isHidden = IsVerify
+            }
+            if let  tips = myConcernCollectionData?.tips{
+                tipsButton.isHidden = tips
+            }
         }
     }
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        tipsButton.layer.borderWidth = 1
+        tipsButton.layer.borderColor = UIColor.white.cgColor
     }
 
 }
